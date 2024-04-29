631b9cda4d6b8ea25a3aeb5dc16f3109ff02b79c
WADA <WADA@DESKTOP-3VFTAVQ>
2024-04-29 19:16:00 +0600

    Add project files.

---
+++ msDcore/Migrations/20240423095247_ittt.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace msDcore.Migrations
+{
+    public partial class ittt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Designation",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Designation",
+                table: "Students");
+        }
+    }
+}
---
+++ msDcore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace msDcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
